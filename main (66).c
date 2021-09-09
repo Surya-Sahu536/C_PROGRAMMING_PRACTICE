af603c17bb5f533c5c3bb5b648e203b43015fafc
Surya-Sahu536 <84575854+Surya-Sahu536@users.noreply.github.com>
2021-09-09 23:58:08 +0530

    Add files via upload

---
+++ main (66).c
@@ -0,0 +1,24 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+C#, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    int a,b,c,d,e,f;
+    //printf("Hello World");
+    a=5||6;
+    b=6&&0;
+    c=2|3;
+    d=2^3;
+    e=2&3;
+    f=!2;
+    printf("%d %d %d %d %d %d",a,b,c,d,e,f);
+
+    return 0;
+}
