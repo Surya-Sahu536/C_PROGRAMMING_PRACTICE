52d85914da41de2c30c58fdddfadc85383a6cbd0
Surya-Sahu536 <84575854+Surya-Sahu536@users.noreply.github.com>
2021-09-10 01:17:36 +0530

    Add files via upload

---
+++ main (68).c
@@ -0,0 +1,20 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+C#, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+   // printf("Hello World");
+   int a,b,c,d,e;
+   a=b=5;
+   printf("%d %d",a,b);
+   //c=!d=5;                                         error: lvalue required as left operand of assignment
+   //printf("%d %d",c,d);
+    return 0;
+}
